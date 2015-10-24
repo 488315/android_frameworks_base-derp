5b3a476371dee0913e9570d6d67975d451ea7ed1
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-10-23 23:44:43 -0400

    Hide power menu on secure lockscreen [1/2]
    Squashed with:
    
        Author: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 29 Jan 2023 22:59:06 +0530
        Fix disabling power menu completely on secured locscreen
    
        * Fixes: crdroidandroid/issue_tracker#107
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6663,6 +6663,12 @@ public final class Settings {
         public static final String HAPTIC_ON_QS = "haptic_on_qs";
 
         /**
+         * Whether to show power menu on LockScreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
