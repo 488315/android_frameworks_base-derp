ebc73f52867f3a15eb0664b7356629e5b2e0f448
Andrzej Perczak <linux@andrzejperczak.com>
2022-09-25 13:53:43 +0200

    biometrics: Fix udfps races
    onFingerUp() and onFingerDown() are racey, presumably they can execute
    at the same time causing strange behaviours. One of it is animation
    glitch left after unlock which causes unresponsive touch around fp reader.
    
    Let's synchronize those methods and also remove quirky exclusivity
    mechanism from UdfpsAnimation.
    
    With such approach most of strange udfps-around bugs should be solved.
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -1017,7 +1017,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         return mSensorProps.sensorType == FingerprintSensorProperties.TYPE_UDFPS_ULTRASONIC;
     }
 
-    public boolean isFingerDown() {
+    public synchronized boolean isFingerDown() {
         return mOnFingerDown;
     }
 
@@ -1027,7 +1027,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mLatencyTracker.onActionEnd(LatencyTracker.ACTION_UDFPS_ILLUMINATE);
     }
 
-    private void onFingerDown(
+    private synchronized void onFingerDown(
             long requestId,
             int x,
             int y,
@@ -1046,7 +1046,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 false /* isAod */);
     }
 
-    private void onFingerDown(
+    private synchronized void onFingerDown(
             long requestId,
             int pointerId,
             float x,
@@ -1085,7 +1085,6 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
             mDeviceEntryFaceAuthInteractor.onUdfpsSensorTouched();
         }
-        mOnFingerDown = true;
         mFingerprintManager.onPointerDown(requestId, mSensorProps.sensorId, pointerId, x, y,
                 minor, major, orientation, time, gestureStart, isAod);
         Trace.endAsyncSection("UdfpsController.e2e.onPointerDown", 0);
@@ -1104,12 +1103,13 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 cb.onFingerDown();
             }
         }
-        if (mUdfpsAnimation != null) {
+        if (!mOnFingerDown && mUdfpsAnimation != null) {
             mUdfpsAnimation.show();
         }
+        mOnFingerDown = true;
     }
 
-    private void onFingerUp(long requestId, @NonNull View view) {
+    private synchronized void onFingerUp(long requestId, @NonNull View view) {
         onFingerUp(
                 requestId,
                 view,
@@ -1124,7 +1124,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 false /* isAod */);
     }
 
-    private void onFingerUp(
+    private synchronized void onFingerUp(
             long requestId,
             View view,
             int pointerId,
