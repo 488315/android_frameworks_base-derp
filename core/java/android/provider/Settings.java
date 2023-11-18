4a4056af50760276d8b07b389d11643daed2fa03
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-19 00:40:47 +0800

    SystemUI: Implement Island notification feature [SQUASHED]
    * slimmed down version of reticker
    
    Squashed:
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Wed, 22 Nov 2023 18:58:49 +0800
        Subject: [PATCH] Island: Update UI Specs and make layout dynamic
    
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Wed, 22 Nov 2023 20:15:45 +0800
        Subject: [PATCH] Island: Add quick call answer/reject click functionalities
    
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Sat, 25 Nov 2023 08:01:10 +0800
        Subject: [PATCH] Island: Implement Dynamic Island effects
    
        * includes notification title handling to prevent redundant notification title headings
        * inspired from IOS' Dynamic Island this makes the island notification to initially show a brief summary of the app notification.
          When the user long click/tap the island notification it will expand and show more info about the app notification.
    
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Sun, 26 Nov 2023 11:39:46 +0800
        Subject: [PATCH] Island: Improvements and fixes
    
        * add support for dynamic icon loading
        * add text overflow handling
        * fix dynamic island effects graphical issues
        * fix and improve tap/click events
        * improve animation performance
    
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
        From: someone5678 <someone5678@users.noreply.github.com>
        Date: Fri, 8 Dec 2023 04:23:12 +0900
        Subject: [PATCH] Island: Overrides showIsland in ShadeViewControllerEmptyImpl
    
        * Adapt to A14 QPR1 changes
    
        Change-Id: Id465ceaa0723d90066c4189b25ecbc13e60ebe77
    
    Co-authored-by: Dil3mm4 <dil3mm4.dev@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6817,6 +6817,11 @@ public final class Settings {
         public static final String HIDE_LOCKSCREEN_STATUS_BAR = "hide_lockscreen_status_bar";
 
         /**
+         * @hide
+         */
+        public static final String ISLAND_NOTIFICATION = "island_notification";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
