DerpFest-bullhead/frameworks_base

Revert "[sb] remove unused param from initializer callback"
This reverts commit d40257cfd6d35ad5a398796cd17d6ba38bba0ec5.

Needed for "SystemUI: Port statusbar brightness control".

Change-Id: I6add5c20a02f5c83c7a5da907fada5265ff32abc

packages/SystemUI/src/com/android/systemui/statusbar/core/StatusBarInitializer.kt
@@ -22,6 +22,7 @@ import com.android.systemui.res.R
 import com.android.systemui.statusbar.core.StatusBarInitializer.OnStatusBarViewInitializedListener
 import com.android.systemui.statusbar.core.StatusBarInitializer.OnStatusBarViewUpdatedListener
 import com.android.systemui.statusbar.phone.PhoneStatusBarTransitions
+import com.android.systemui.statusbar.phone.PhoneStatusBarView
 import com.android.systemui.statusbar.phone.PhoneStatusBarViewController
 import com.android.systemui.statusbar.phone.fragment.CollapsedStatusBarFragment
 import com.android.systemui.statusbar.phone.fragment.dagger.StatusBarFragmentComponent
@@ -59,6 +60,7 @@ interface StatusBarInitializer {
 
     interface OnStatusBarViewUpdatedListener {
         fun onStatusBarViewUpdated(
+            statusBarView: PhoneStatusBarView,
             statusBarViewController: PhoneStatusBarViewController,
             statusBarTransitions: PhoneStatusBarTransitions,
         )
@@ -86,6 +88,7 @@ constructor(
                             (fragment as CollapsedStatusBarFragment).statusBarFragmentComponent
                                 ?: throw IllegalStateException()
                         statusBarViewUpdatedListener?.onStatusBarViewUpdated(
+                            statusBarFragmentComponent.phoneStatusBarView,
                             statusBarFragmentComponent.phoneStatusBarViewController,
                             statusBarFragmentComponent.phoneStatusBarTransitions,
                         )

488315/android_frameworks_base-derp

packages/SystemUI/src/com/android/systemui/statusbar/core/StatusBarInitializer.kt
@@ -67,6 +67,7 @@ interface StatusBarInitializer : CoreStartable {
 
     interface OnStatusBarViewUpdatedListener {
         fun onStatusBarViewUpdated(
+            statusBarView: PhoneStatusBarView,
             statusBarViewController: PhoneStatusBarViewController,
             statusBarTransitions: PhoneStatusBarTransitions,
         )
@@ -106,6 +107,7 @@ constructor(
             // If a listener is added after initialization, immediately call the callback
             component?.let { component ->
                 field?.onStatusBarViewUpdated(
+                    component.phoneStatusBarView,
                     component.phoneStatusBarViewController,
                     component.phoneStatusBarTransitions,
                 )
@@ -148,6 +150,7 @@ constructor(
                             component.init()
 
                             statusBarViewUpdatedListener?.onStatusBarViewUpdated(
+                                component.phoneStatusBarView,
                                 component.phoneStatusBarViewController,
                                 component.phoneStatusBarTransitions,
                             )
@@ -183,6 +186,7 @@ constructor(
                         }
                         component = fragment.homeStatusBarComponent
                         statusBarViewUpdatedListener?.onStatusBarViewUpdated(
+                            component!!.phoneStatusBarView,
                             component!!.phoneStatusBarViewController,
                             component!!.phoneStatusBarTransitions,
                         )